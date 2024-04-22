af5292831f4760c5dd0bca6b021de17b9bda3809
FlyingButteryTuna <tsekh.arseniy@yandex.ru>
2024-04-22 21:39:22 +0300

    parsers: Refractor fetching funcs into a type

--- parsers/fetchers.go
+++ parsers/fetchers.go
@@ -10,14 +10,22 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func FetchPage(url string, client *http.Client) (*http.Response, error) {
+type Fetcher struct {
+	client *http.Client
+}
+
+func NewFetcher(client *http.Client) *Fetcher {
+	return &Fetcher{client: client}
+}
+
+func (p *Fetcher) FetchPage(url string) (*http.Response, error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create request: %w", err)
 	}
 	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36")
 
-	resp, err := client.Do(req)
+	resp, err := p.client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("failed to perform request: %w", err)
 	}
@@ -30,7 +38,7 @@ func FetchPage(url string, client *http.Client) (*http.Response, error) {
 	return resp, nil
 }
 
-func SaveAllChapters(novelData *novel.NovelData, parser NovelParser, novelDirPath string, client *http.Client) error {
+func (p *Fetcher) SaveAllChapters(novelData *novel.NovelData, parser NovelParser, novelDirPath string) error {
 	_, err := os.Stat(novelDirPath)
 	if os.IsNotExist(err) {
 		err = os.Mkdir(novelDirPath, 0755)
@@ -43,7 +51,7 @@ func SaveAllChapters(novelData *novel.NovelData, parser NovelParser, novelDirPat
 		for _, chapter := range section.Chapters {
 			fullChapterLink := novelData.Link + chapter.Link
 
-			resp, err := FetchPage(fullChapterLink, client)
+			resp, err := p.FetchPage(fullChapterLink)
 			if err != nil {
 				return err
 			}
